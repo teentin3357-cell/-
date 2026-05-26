341f58146a5f9de17ff3e009c2ffafae21b103fb
teentin3357-cell <teentin3357@gmail.com>
2026-05-26 10:18:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ if st.button("숫자 뽑기", type="primary"):
  st.success("🎉 추출된 숫자입니다!")
  cols = st.columns(5)
  for i, num in enumerate(random_numbers):
- cols[i].metric(label=f"{i+1}번째 숫자", value=num)
+  cols[i].metric(label=f"{i+1}번째 숫자", value=num)
