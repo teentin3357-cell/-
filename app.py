618cf9a5b0928fb632ba6e8a2bf4f2a67d55b549
teentin3357-cell <teentin3357@gmail.com>
2026-05-26 10:17:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import random
 import streamlit as st
 if st.button("숫자 뽑기", type="primary"):
-random_numbers = random.sample(range(1, 101), 5)
-random_numbers.sort()
-st.success("🎉 추출된 숫자입니다!")
-cols = st.columns(5)
+ random_numbers = random.sample(range(1, 101), 5)
+ random_numbers.sort()
+ st.success("🎉 추출된 숫자입니다!")
+ cols = st.columns(5)
     for i, num in enumerate(random_numbers):
       cols[i].metric(label=f"{i+1}번째 숫자", value=num)
